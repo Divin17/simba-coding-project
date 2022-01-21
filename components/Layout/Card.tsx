1f2f6a9163762041650d5711b65ebdfdf94f4393
ISHIMWE Divin <divinfiston1@gmail.com>
2022-01-22 01:07:02 +0200

    fixing balance card types

--- components/Layout/Card.tsx
+++ components/Layout/Card.tsx
@@ -10,7 +10,7 @@ export type Props = {
 const Card: React.FC<Props> = (props) => {
    return (
       <div className='flex gap-4 bg-white p-4 border rounded-lg'>
-         <Image src={props.path} width={60} height={60} alt={props.balance} />
+         <Image src={props.path} width={60} height={60} alt={props.name} />
          <div className='flex flex-col justify-around'>
             <p className='font-bold text-green-400'>{props.name}</p>
             <p className='text-black font-semibold'>{props.balance}</p>
